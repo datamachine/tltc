8d6efc77da42934a094a2cd18847f51e6040a643
Phillip Lopo <philliplopo@gmail.com>
2015-06-19 16:15:58 -0700

    creating dedicated base and builtin types for python34 types

--- tlcl/targets/python34/base.py
+++ tlcl/targets/python34/base.py
@@ -366,18 +366,3 @@ class Double(_Real, TLObject):
     def serialize(self):
         return Double._struct.pack(self._data)
 Double.register(float)
-
-"""
-"""
-class Vector(TLObject):
-    def __init__(self, vector_type):
-        if not isinstance(vector_type, type):
-            raise TypeError('vector_type must be a type')
-        self._type = vector_type
-        self._data = []
-
-    def serialize(self):
-        result = b''
-        for d in self._data:
-            result += d.serialize()
-        return result
