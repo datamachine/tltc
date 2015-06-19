54492da0eec19b30492dd0f3508ebae9f2563c1e
Phillip Lopo <philliplopo@gmail.com>
2015-06-19 12:06:29 -0700

    Created IRCombinator

---
+++ tlcl/ir/combinator.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class IRCombinator:
+	def __init__(self, identifer, number, params, result_type):
+		self.identifer = identifer
+		self.number = number
+		self.params = params
+		self.result_type = result_type
