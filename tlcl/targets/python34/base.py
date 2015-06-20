516369eabec7e3c38e9f5c77cb93830b30228792
Phillip Lopo <philliplopo@gmail.com>
2015-06-20 16:39:47 -0700

    Base types act a lot more friendly now

--- tlcl/targets/python34/base.py
+++ tlcl/targets/python34/base.py
@@ -17,8 +17,6 @@ class TLType(metaclass=ABCMeta):
     def __init__(self):
         self._combinators = {}
 
-
-
 class Combinator(metaclass=ABCMeta):
     _combinators = {}
 
@@ -37,205 +35,198 @@ class Combinator(metaclass=ABCMeta):
         raise NotImplemented
 
 """
-Int https://core.telegram.org/type/int
+_Integral utility base class used by Integral base type
 """
-class Int(numbers.Integral, TLObject):
-    _struct = struct.Struct('!L')
-
-    def __init__(self, _int):
-        self._int = int(_int)
-
-    @property
-    def _tl_type_class(self):
-        return type(self)
+class _Integral(numbers.Integral, TLObject):
+    def __init__(self, _data):
+        self._data = type(self)._cls(_data)
 
     def serialize(self):
-        return Int._struct.pack(self._int)
-
-    def __repr__(self):
-        return 'Int({})'.format(self._int)
+        return type(self)._cls._struct.pack(self._data)
 
     def __bytes__(self):
-        return Int._struct.pack(self._int)
+        return self.serialize()
 
     def __format__(self, format_spec):
         if 'x' in format_spec or 'b' in format_spec:
             return format(struct.unpack('<L', self.serialize())[0], format_spec)
-        return format(self._int, format_spec)
+        return format(self._data, format_spec)
 
     """
     Abstract methods from numbers.Integral
     """
     def __int__(self):
-        return self._int
+        return self._data.__int__()
 
     def __pow__(self, exponent, modulus=None):
-        if modulus:
-            return self._tl_type_class(self._int.__pow__(exponent, modulus=modulus))
-        else:
-            return self._tl_type_class(self._int.__pow__(exponent))
+        return self._data.__pow__(exponent, modulus)
 
     def __lshift__(self, other):
-        return self._tl_type_class(self._int.__lshift__(other))
+        return self._data.__lshift__(other)
 
     def __rlshift__(self, other):
-        return self._tl_type_class(self._int.__rlshift__(other))
+        return self._data.__rlshift__(other)
 
     def __rshift__(self, other):
-        return self._tl_type_class(self._int.__rshift__(other))
+        return self._data.__rshift__(other)
 
     def __rrshift__(self, other):
-        return self._tl_type_class(self._int.__rrshift__(other))
+        return self._data.__rrshift__(other)
 
     def __and__(self, other):
-        return self._tl_type_class(self._int.__and__(other))
+        return self._data.__and__(other)
 
     def __rand__(self, other):
-        return self._tl_type_class(self._int.__rand__(other))
+        return self._data.__rand__(other)
 
     def __xor__(self, other):
-        return self._tl_type_class(self._int.__xor__(other))
+        return self._data.__xor__(other)
 
     def __rxor__(self, other):
-        return self._tl_type_class(self._int.__rxor__(other))
+        return self._data.__rxor__(other)
 
     def __or__(self, other):
-        return self._tl_type_class(self._int.__or__(other))
+        return self._data.__or__(other)
 
     def __ror__(self, other):
-        return self._tl_type_class(self._int.__ror__(other))
+        return self._data.__ror__(other)
 
     def __invert__(self):
-        return self._tl_type_class(self._int.__invert__())
+        return self._data.__invert__(other)
 
     """
     Abstract methods from numbers.Real
     """        
     def __trunc__(self):
-        return self._tl_type_class(self._int.__trunc__())
+        return self._data.__trunc__(other)
 
     def __floor__(self):
-        return self._tl_type_class(self._int.__floor__())
+        return self._data.__floor__(other)
 
     def __ceil__(self):
-        return self._tl_type_class(self._int.__ceil__())
+        return self._data.__ceil__(other)
 
     def __round__(self, ndigits=None):
-        return self._tl_type_class(self._int.__round__(ndigits=ndigits))
+        return self._data.__round__(ndigits)
 
     def __floordiv__(self, other):
-        return self._tl_type_class(self._int.__floordiv__(other))
+        return self._data.__floordiv__(other)
 
     def __rfloordiv__(self, other):
-        return self._tl_type_class(self._int.__rfloordiv__(other))
+        return self._data.__rfloordiv__(other)
         
     def __mod__(self, other):
-        return self._tl_type_class(self._int.__mod__(other))
+        return self._data.__mod__(other)
         
     def __rmod__(self, other):
-        return self._tl_type_class(self._int.__rmod__(other))
+        return self._data.__rmod__(other)
         
     def __lt__(self, other):
-        return self._int.__lt__(other)
+        return self._data.__lt__(other)
         
     def __le__(self, other):
-        return self._int.__le__(other)
+        return self._data.__le__(other)
 
 
     """
     Abstract methods from numbers.Complex
     """
     def __add__(self, other):
-        return self._tl_type_class(self._int.__add__(other))
+        return self._data.__add__(other)
 
     def __radd__(self, other):
-        return self.classtype(self._int.__radd__(other))
+        return self._data.__radd__(other)
 
     def __neg__(self):
-        return self._tl_type_class(self._int.__neg__())
+        return self._data.__neg__()
 
     def __pos__(self):
-        return self._tl_type_class(self._int.__pos__())
+        return self._data.__pos__()
 
     def __mul__(self, other):
-        return self._tl_type_class(self._int.__mul__(other))
+        return self._data.__mul__(other)
 
     def __rmul__(self, other):
-        return self._tl_type_class(self._int.__rmul__(other))
+        return self._data.__rmul__(other)
 
     def __truediv__(self, other):
-        return self._tl_type_class(self._int.__truediv__(other))
+        return self._data.__truediv__(other)
 
     def __rtruediv__(self, other):
-        return self._tl_type_class(self._int.__rtruediv__(other))
+        return self._data.__rtruediv__(other)
 
     def __rpow__(self, base):
-        return self._tl_type_class(self._int.__rpow__(exponent))
+        return self._data.__rpow__(base)
 
     def __abs__(self):
-        return self._tl_type_class(self._int.__abs__())
+        return self._data.__abs__()
 
     def __eq__(self, other):
-        return self._int.__eq__(other)
-Int.register(int)
+        return self._data.__eq__(other)
+
 
 """
-Long https://core.telegram.org/type/long
+BaseInt https://core.telegram.org/type/int
 """
-class Long(Int, TLObject):        
+class BaseInt(_Integral):
+    _cls = int
+    _struct = struct.Struct('!l')
+
+    def __init__(self, _int):
+        super().__init__(_int)
+
+    def serialize(self):
+        return BaseInt._struct.pack(self._data)
+
+    def __repr__(self):
+        return 'BaseInt({:d})'.format(self._data)
+
+    def __bytes__(self):
+        return self.serialize()
+
+    def __format__(self, format_spec):
+        if 'x' in format_spec or 'b' in format_spec:
+            return format(struct.unpack('<L', self.serialize())[0], format_spec)
+        return format(self._data, format_spec)
+BaseInt.register(BaseInt._cls)
+
+"""
+BaseLong https://core.telegram.org/type/long
+"""
+class BaseLong(_Integral):
+    _cls = int     
     _struct = struct.Struct('!q')
 
     def __init__(self, _long):
-        self._int = int(_long)
+        super().__init__(_long)
 
-    @property
-    def _internal_data_type(self):
-        return int
-
-    @property
-    def _tl_type_class(self):
-        return type(self)
+    def __init__(self, _long):
+        self._data = int(_long)
 
     def serialize(obj):
-        return Long._struct.pack(self._int)
-Long.register(int)
+        return BaseLong._struct.pack(self._data)
+BaseLong.register(BaseLong._cls)
 
 """
-Bool https://core.telegram.org/type/bool
+BaseBool https://core.telegram.org/type/bool
 """
-class Bool(Int, TLObject):
+class BaseBool(_Integral):
+    _cls = bool
     _struct = struct.Struct('!q')
 
     def __init__(self, _bool):
-        self._int = bool(_bool)
-
-    @property
-    def internal_data_type(self):
-        return bool
-
-    @property
-    def _tl_type_class(self):
-        return type(self)
+        super().__init__(_bool)
 
     def serialize(obj):
-        return Bool._struct.pack(self._int)
-
-Bool.register(bool)
+        return BaseBool._struct.pack(self._data)
+BaseBool.register(BaseBool._cls)
 
 class Double(numbers.Real, TLObject):
     _struct = struct.Struct('!d')
 
     def __init__(self, _double):
-        self._int = float(_double)
-
-    @property
-    def _internal_data_type(self):
-        return float 
-
-    @property
-    def _tl_type_class(self):
-        return type(self) 
+        super().__init__(_double)
 
     def serialize(self):
-        return Double._struct.pack(self._int)
+        return Double._struct.pack(self._data)
 Double.register(float)
