1fbd55d76ac334c1f8cc36ba35093815cb4d36a7
Phillip Lopo <philliplopo@gmail.com>
2015-06-20 19:00:37 -0700

    descibing identifer kind in IR

--- tlcl/ir/type.py
+++ tlcl/ir/type.py
@@ -1,7 +1,14 @@
 from enum import Enum
+import re
 
 class IRType:
     _IRTypeKind = Enum('IRTypeKind', ['BOXED', 'BARE'])
+    BOXED = _IRTypeKind.BOXED
+    BARE = _IRTypeKind.BARE
 
     def __init__(self, identifier):
+        if re.match('[a-z]', identifier.ident):
+            self.kind = IRType.BARE
+        else:
+            self.kind = IRType.BOXED
         self.identifier = identifier
