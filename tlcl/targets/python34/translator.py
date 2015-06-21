b4f23908770f38bceaf14a0148b3c2aee1354e50
Phillip Lopo <philliplopo@gmail.com>
2015-06-20 21:53:00 -0700

    Removing python34 translate object

--- tlcl/targets/python34/translator.py
+++
@@ -1,9 +0,0 @@
-from tlcl.translator import TLTranslator
-
-class Python34Translator(TLTranslator):
-    def translate(self):
-        pass
-        #self.define_builtins()
-        #self.define_types()
-        #self.define_constructors()
-        #self.define_functions()
