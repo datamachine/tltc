d56f76b93146befb788a7518021763cc24ae74e5
Phillip Lopo <philliplopo@gmail.com>
2015-06-22 12:47:43 -0700

    Making IRType member more consistent with othe IR classes

--- tlcl/ir/type.py
+++ tlcl/ir/type.py
@@ -6,15 +6,20 @@ class IRType:
     BARE = _IRTypeKind.BARE
     NAT = _IRTypeKind.NAT
 
-    def __init__(self, identifier):
-        if identifier.is_boxed():
+    def __init__(self, ir_ident):
+        if ir_ident.is_boxed():
             self.kind = IRType.BOXED
         else:
             self.kind = IRType.BARE
 
-        self.identifier = identifier
+        self._ir_ident = ir_ident
         self.constructors = []
         self.functions = []
+
+    @property
+    def identifier(self):
+        return self._ir_ident.identifier
+    
 
     def __repr__(self):
         return '<IRType: kind={}, identifier={}>'.format(self.kind, self.identifier.full_ident)
