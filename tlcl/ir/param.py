5529a164b01007dbc1d9edb46462067d7a76a300
Phillip Lopo <philliplopo@gmail.com>
2015-06-22 12:50:13 -0700

    Making IRType member more consistent with othe IR classes

--- tlcl/ir/param.py
+++ tlcl/ir/param.py
@@ -8,14 +8,18 @@ class IRParameter:
     ARG_NAT = _IRParameterKind.ARG_NAT
     MULT = _IRParameterKind.MULT
 
-    def __init__(self, kind, param_ident, arg_type):
-        self.kind = IRParameter._IRParameterKind(kind)
-        self.param_ident = param_ident
-        self.arg_type = arg_type
+    def __init__(self, kind, ir_ident, arg_type):
+        self._kind = IRParameter._IRParameterKind(kind)
+        self._ir_ident = ir_ident
+        self._arg_type = arg_type
+
+    @property
+    def identifier(self):
+        return self._ir_ident.identifier
 
     def __repr__(self):
-        fmt='<IRParameter: kind={}, param_ident={}, arg_type={}>'
-        return fmt.format(self.kind, self.param_ident, self.arg_type.identifier)
+        fmt='<IRParameter({}): kind={}, arg_type={}>'
+        return fmt.format(self, self.kind, self.arg_type)
 
     def __str__(self):
-        return '{}:{}'.format(self.param_ident, self.arg_type.identifier)
+        return '{}:{}'.format(self.identifier, self.arg_type)
