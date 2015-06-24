d919b6f4b8f52957830d0c66ff13c512a42701e1
Phillip Lopo <philliplopo@gmail.com>
2015-06-23 22:38:07 -0700

    generating tl.py now

--- gen_test.sh
+++ gen_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3.4 -m tlcl.compile -t Python3.4 schemas/testgen.tl
+python3.4 -m tlcl.compile -t Python3.4 schemas/testgen.tl > tl.py
 
--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3.4
 
 import io
-import py34types as py3
+import tl
 
 def test_int():
     val = int(1234)
 
 
-    _bytes = bytearray(py3.int_c.number)
+    _bytes = bytearray(tl.int_c.number)
     _bytes += val.to_bytes(4, byteorder='little')
 
     io_bytes = io.BytesIO(_bytes)
-    result = py3.deserialize(io_bytes)
+    result = tl.deserialize(io_bytes)
 
     assert len(io_bytes.read()) == 0, 'Failed to read all the data'
     assert val == result, 'Error deserializing int(%d)' %val
@@ -20,11 +20,11 @@ def test_int():
 def test_long():
     val = int(12345678)
 
-    _bytes = bytearray(py3.long_c.number)
+    _bytes = bytearray(tl.long_c.number)
     _bytes += val.to_bytes(8, byteorder='little')
 
     io_bytes = io.BytesIO(_bytes)
-    result = py3.deserialize(io_bytes)
+    result = tl.deserialize(io_bytes)
 
     assert len(io_bytes.read()) == 0, 'Failed to read all the data'
     assert val == result, 'Error deserializing int(%d)' %val
@@ -32,11 +32,11 @@ def test_long():
 def test_double():
     val = float(94.9)
 
-    _bytes = bytearray(py3.double_c.number)
-    _bytes += py3.double_c._struct.pack(val)
+    _bytes = bytearray(tl.double_c.number)
+    _bytes += tl.double_c._struct.pack(val)
 
     io_bytes = io.BytesIO(_bytes)
-    result = py3.deserialize(io_bytes)
+    result = tl.deserialize(io_bytes)
 
     assert len(io_bytes.read()) == 0, 'Failed to read all the data'
     assert val == result, 'Error deserializing float(%f)' %val
@@ -44,17 +44,17 @@ def test_double():
 def test_string():
     val = 'Peter'
 
-    _bytes = bytearray(py3.string_c.number)
+    _bytes = bytearray(tl.string_c.number)
     _bytes += b'\x05' + val.encode() + b'\x00\x00'
 
     io_bytes = io.BytesIO(_bytes)
-    result = py3.deserialize(io_bytes)
+    result = tl.deserialize(io_bytes)
 
     assert len(io_bytes.read()) == 0, 'Failed to read all the data'
     assert result == 'Peter', 'Error reading %s' %_bytes
 
     val = 'Michael'
-    result = py3.serialize(py3.string_c, val)
+    result = tl.serialize(tl.string_c, val)
 
     assert result == b'\x07Michael', 'failed to serialize string'
     assert len(result)%4 == 0, 'string is not properly aligned'
@@ -62,17 +62,17 @@ def test_string():
 def test_bytes():
     val = b'Parker'
 
-    _bytes = bytearray(py3.bytes_c.number)
+    _bytes = bytearray(tl.bytes_c.number)
     _bytes += b'\x06' + val + b'\x00'
 
     io_bytes = io.BytesIO(_bytes)
-    result = py3.deserialize(io_bytes)
+    result = tl.deserialize(io_bytes)
 
     assert len(io_bytes.read()) == 0, 'Failed to read all the data'
     assert result == val, 'Error reading %s' %_bytes
 
     val = b'Jordan-Martinez'
-    result = py3.serialize(py3.bytes_c, val)
+    result = tl.serialize(tl.bytes_c, val)
 
     assert result == b'\x0FJordan-Martinez', 'failed to serialize bytes %s' %result
     assert len(result)%4 == 0, 'bytes are not properly aligned'
